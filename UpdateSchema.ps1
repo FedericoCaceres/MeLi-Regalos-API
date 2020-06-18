0fb582b832137111fae42b7e23b0acf22b55b17e
Federico Cáceres <fedecaceres.developer@gmail.com>
2020-06-18 12:05:53 -0300

    Schema updated - jueves 06/18/2020 12:03 -03:00

---
+++ UpdateSchema.ps1
@@ -0,0 +1,6 @@
+﻿(Get-Culture).DateTimeFormat
+$Time = Get-Date -Format "dddd MM/dd/yyyy HH:mm K"
+$Mensaje = "Schema updated: "
+git add .
+git commit -m "$($Mensaje) - $($Time)"
+git push -u origin master
