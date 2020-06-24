a31ca96ded9a55f6c7bb9dc3fc9ba518d008f236
Federico Cáceres <fedecaceres.developer@gmail.com>
2020-06-24 15:04:47 -0300

    Schema updated:  miércoles 06/24/2020 15:04

--- UpdateSchema.ps1
+++ UpdateSchema.ps1
@@ -1,6 +1,6 @@
 ﻿cd "C:\Users\fedec\Programacion\Recursos para programar\GraphQL"
 graphql get-schema
-$Time = Get-Date -Format "dddd MM/dd/yyyy HH:mm K"
+$Time = Get-Date -Format "dddd MM/dd/yyyy HH:mm"
 $Mensaje = "Schema updated: "
 git add .
 git commit -m "$($Mensaje) $($Time)"
